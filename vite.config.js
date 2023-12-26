3f1a602d39e794b42cedcc18f8b3020004d09c39
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-26 09:59:23 +0545

    fix: build file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './ts-toDos/',
+  base: './', // Sets the base path for the project
+
+  // Other Vite configurations...
 });
